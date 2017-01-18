c60b54dad828d1c610c5681ce332b5106d78fef5
etbeur <romain.etheve@gmail.com>
2017-01-18 09:36:04 +0100

    ajout dossier branche romain

--- api/src/AppBundle/Controller/DefaultController.php
+++ api/src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,17 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/login", name="loginPage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $test = array('login'=> 'admin', 'password' => "admin");
+        return new JsonResponse($test);
     }
 }
--- api/src/AppBundle/Model/Eleve.php
+++ api/src/AppBundle/Model/Eleve.php
@@ -11,5 +11,22 @@ namespace AppBundle\Model;
 
 class Eleve
 {
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $birthDate;
+    public $adress;
+    public $phone;
+    public $email;
+    public $emergencyContact;
+    public $github;
+    public $likedIn;
+    public $personalProject;
+    public $photo;
 
+    function __construct(int $id, String $firstname, String $lastname, \dateTime $birthDate,
+                        String $adress, String $phone, String $email, g )
+    {
+
+    }
 }
