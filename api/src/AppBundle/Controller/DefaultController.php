fd99fc3c7963a0e662e1b99820753e97c82b149f
etbeur <romain.etheve@gmail.com>
2017-01-19 22:42:55 +0100

    modif des controller et nettoyage code

--- api/src/AppBundle/Controller/DefaultController.php
+++ api/src/AppBundle/Controller/DefaultController.php
@@ -51,17 +51,14 @@ class DefaultController extends Controller
 //          On crée un tableau pour stocker les infos de la table
             $infosUser = [];
             foreach ($identifiants as $identifiant) {
-                if ($identifiant->getLogin() == $login & $identifiant->getPassword() == $password)
-                {
+                if ($identifiant->getLogin() == $login & $identifiant->getPassword() == $password) {
                     $infosUser = [
                         'login' => $identifiant->getLogin(),
                         'id' => $identifiant->getId(),
                         'firstname' => $identifiant->getFirstname(),
                         'lastname' => $identifiant->getlastname()
                     ];
-                }
-                else
-                {
+                } else {
                     $false = array
                     (
                         'responseServer' => "utilisateur non reconnu",
@@ -71,105 +68,9 @@ class DefaultController extends Controller
             }
 
             return new JsonResponse($infosUser);
-
-//          Vu twig pour test
-//            return $this->render('default/test.html.twig', [
-//                'name' => new JsonResponse($infosUser)
-//            ]);
         } else {
             $false = array('aucune reponse possible' => 'data non valide');
             return new JsonResponse($false);
         }
     }
-//        $test = array('login'=> 'admin', 'password' => "admin");
-//        return new JsonResponse($test);
-
-
-    /**
-     * @Route("/", name="homepage")
-     */
-
-    public function testAction(Request $request) {
-
-//        On créé un nouveau Student
-
-        $student = new Student();
-        $student->setFirstname("paul");
-        $student->setLastname("dupont");
-        $student->setBirthDate(new \DateTime(1995-07-20));
-        $student->setAddress("8Rue du commandant Charcot");
-        $student->setPhone("060610630606");
-        $student->setEmail("paula.dupont@gmail.com");
-
-//        On récupère dans la DB les données que l'on veut
-
-        $skillStudent = $this->getDoctrine()
-            ->getRepository('AppBundle:Skill')
-            ->findBy(
-                array('name' => "Python"),
-                array("name" => "Django"),
-                array("name" => "Drupal"),
-                array("name" => "HTML5"),
-                array("name" => "CSS3"),
-                array("name" => "MySQL"),
-                array("name" => "Developpement Back-End"),
-                array("name" => "Javascript")
-            );
-
-        dump($skillStudent);
-
-//        On ajoute au nouveau student les compétences recupérées dans la DB
-        $student->addSkill($skillStudent);
-
-//       On envoie les données à la DB
-        $this->getDoctrine()->getEntityManager()->persist($student);
-        $this->getDoctrine()->getEntityManager()->flush();
-
-
-        return new Response("Test OK");
-
-    }
-
-    /**
-     * @Route("/testLog", name="loginPage")
-     */
-
-    public function testLoginAction(Request $request)
-    {
-
-//          Récupération des données correspondant au login de connexion si correspondance
-            $identifiants = $this->getDoctrine()
-                ->getRepository('AppBundle:user')
-                ->findAll();
-            /* @var $identifiant user */
-
-//          Si identifiants n'existent pas dans la table
-            if (!$identifiants) {
-                throw $this->createNotFoundException(
-                    'No login found for ' . $identifiants
-                );
-            }
-
-//          On crée un tableau pour stocker les infos de la table
-            $infosUser = [];
-            foreach ($identifiants as $identifiant) {
-                    $infosUser = [
-                        'login' => $identifiant->getLogin(),
-                        'id' => $identifiant->getId(),
-                        'firstname' => $identifiant->getFirstname(),
-                        'lastname' => $identifiant->getLastname()
-                    ];
-            }
-
-//          Vu twig pour test
-            return $this->render('default/test.html.twig', [
-                'name' => new JsonResponse($infosUser)
-            ]);
-
-    }
-//        $test = array('login'=> 'admin', 'password' => "admin");
-//        return new JsonResponse($test);
-
-
-
 }
