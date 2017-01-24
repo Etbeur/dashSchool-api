c01f6457eaa0024dd469247c007a3e91a6a5782d
Solenn Baer <solenn.baer@gmail.com>
2017-01-24 23:26:16 +0100

    modification de update, fonctionnele sur server sans les skills

--- api/src/AppBundle/Controller/modifyController.php
+++ api/src/AppBundle/Controller/modifyController.php
@@ -14,79 +14,95 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class modifyController extends Controller
 {
     /**
-     * @Route("/student/update/{id}", name="student", defaults = {"id" = null})
+     * @Route("/student/update/{id}", name="updateStudent", defaults = {"id" = null})
      */
     public function editStudentAction(Request $request)
     {
 
-////        Récupération du Json envoyé par le formulaire, decode pour pouvoir le lire
-//        $dataForm = file_get_contents("php://input");
-//        $data = json_decode($dataForm);
+        //        Récupération du Json envoyé par le formulaire, decode pour pouvoir le lire
+        $dataForm = file_get_contents("php://input");
+        $data = json_decode($dataForm);
 
-//        On récupère le GET envoyé dans l'url
+        //        On récupère le GET envoyé dans l'url
         $id = $request->get('id');
 
-//        On récupère l'Entity manager
+        //        On récupère l'Entity manager
         $em = $this->getDoctrine()->getManager();
 
-//        On récupère le repository Student
+        //        On récupère le repository Student
         $student = $em->getRepository('AppBundle:Student')->findOneBy(array('id' => $id));
 
-        //              On récupère les skills associés à l'étudiant
+        //        On récupère les skills associés à l'étudiant
         $infoSkill = $student->getSkills();
-        dump($infoSkill);
 
-        //            On supprimes toutes les compétences de l'eleve
-        foreach ($infoSkill as $skillDelete){
-            $student->removeSkill($skillDelete);
-        }
-
-        return new Response('test');
-
-
-//
-////        On associe chaque variable aux données envoyées par le form
-//        function missingInput($input){
-//            new JsonResponse("Le champ ".$input." est manquant");
+//        //        On supprime toutes les compétences de l'eleve
+//        foreach ($infoSkill as $skillDelete) {
+//            $student->removeSkill($skillDelete);
 //        }
-//
-//        $firstname = (isset($data->firstname)) ? $data->firstname : missingInput('firstname');
-//        $lastname = (isset($data->lastname)) ? $data->lastname : missingInput('lastname');;
-//        $gender = (isset($data->gender)) ? $data->gender : missingInput('gender');;
-//        $birthDate = (isset($data->birthDate)) ? $data->birthDate : missingInput('birthDate');;
-//        $address = (isset($data->address)) ? $data->address : missingInput('address');;
-//        $phone = (isset($data->phone)) ? $data->phone : missingInput('phone');;
-//        $email = (isset($data->email)) ? $data->email : missingInput('email');;
-//        $emergencyContact = (isset($data->emergencyContact)) ? $data->emergencyContact : null;
-//        $github = (isset($data->github)) ? $data->github : null;
-//        $linkedIn = (isset($data->linkedin)) ? $data->linkedin : null;
-//        $personalProject = (isset($data->personalProject)) ? $data->personalProject : null;
-//        $photo = (isset($data->photo)) ? $data->photo : null;
-////        $newSkills = (isset($data->skills)) ? $data->skills : null;
-//
-////      On modifie l'eleve avec les données fournies
-//        $student->setFirstname($firstname);
-//        $student->setLastname($lastname);
-//        $student->setGender($gender);
-//        $student->setBirthDate(new \DateTime($birthDate));
-//        $student->setAddress($address);
-//        $student->setPhone($phone);
-//        $student->setEmail($email);
-//        $student->setEmergencyContact($emergencyContact);
-//        $student->setGithub($github);
-//        $student->setLinkedIn($linkedIn);
-//        $student->setPersonalProject($personalProject);
-//        $student->setPhoto($photo);
-//
-//
-////        foreach ($newSkills as $skillStudent){
-////            $skill = $em->getRepository('AppBundle:Skill')->findOneBy(array('id' => $skillStudent));
-////            $student->addSkill($skill);
-////        }
-//        $em->flush();
-//
-//        //        Une fois effacement terminé, on verifie si $studentDelete existe encore ou pas dans l'em
-//        return ($student) ? new JsonResponse(array('response' => "student update")) : new JsonResponse(array('response' => "student delete"));
+
+        //        On définit les variables avec ce que l'on récupère du JSON'
+        $firstname = $data->firstname;
+        $lastname = $data->lastname;
+        $gender = $data->gender;
+        $birthDate = $data->birthDate;
+        $address = $data->address;
+        $phone = $data->phone;
+        $email = $data->email;
+        $emergencyContact = $data->emergencyContact;
+        $github = $data->github;
+        $linkedIn = $data->linkedin;
+        $personalProject = $data->personalProject;
+        $photo = $data->photo;
+        $newSkills = $data->skills;
+
+        //        SI les données existent et sont differentes de null, on modifie l'eleve dans la DB, sinon on ne fait rien
+        if (isset($firstname) && ($firstname) != null)
+            $student->setFirstname($firstname);
+
+        if (isset($lastname) && ($lastname) != null)
+            $student->setLastname($lastname);
+
+        if (isset($gender) && ($gender) != null)
+            $student->setgender($gender);
+
+        if (isset($birthDate) && ($data->birthDate) != null)
+            $student->setbirthDate(new \DateTime($birthDate));
+
+        if (isset($address) && ($address) != null)
+            $student->setaddress($address);
+
+        if (isset($phone) && ($phone) != null)
+            $student->setphone($data->phone);
+
+        if (isset($email) && ($email) != null)
+            $student->setemail($email);
+
+        if (isset($emergencyContact) && ($emergencyContact) != null)
+            $student->setemergencyContact($emergencyContact);
+
+        if (isset($github) && ($github) != null)
+            $student->setgithub($github);
+
+        if (isset($linkedIn) && ($linkedIn) != null)
+            $student->setlinkedin($linkedIn);
+
+        if (isset($personalProject) && ($personalProject) != null)
+            $student->setpersonalProject($personalProject);
+
+        if (isset($photo) && ($photo) != null)
+            $student->setphoto($photo);
+
+//        //        Pour chaque id de competence récupéré, on va chercher la compétence dans la table skill et on l'ajoute à l'eleve
+//        foreach ($newSkills as $skillStudent) {
+//            $skill = $em->getRepository('AppBundle:Skill')->findOneBy(array('id' => $skillStudent));
+//            $student->addSkill($skill);
+//        }
+
+        //        On met a jour dans la DB
+        $em->flush();
+
+        //        Une fois effacement terminé, on verifie si $studentDelete existe encore ou pas dans l'em
+        return ($student) ? new JsonResponse(array('response' => "student update")) : new JsonResponse(array('response' => "student dead"));
 
     }
 }
