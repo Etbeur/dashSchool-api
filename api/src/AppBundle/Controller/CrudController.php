8aa949c177a072cceaecb355f21bd2135c643eae
Solenn Baer <solenn.baer@gmail.com>
2017-01-26 10:57:03 +0100

    fusion du controller modify et crud

--- api/src/AppBundle/Controller/CrudController.php
+++ api/src/AppBundle/Controller/CrudController.php
@@ -78,6 +78,99 @@ class CrudController extends Controller
     }
 
     /**
+     * @Route("/student/update/{id}", name="updateStudent", defaults = {"id" = null})
+     * Modification d'un Student
+     */
+    public function editStudentAction(Request $request)
+    {
+
+        //        Récupération du Json envoyé par le formulaire, decode pour pouvoir le lire
+        $dataForm = file_get_contents("php://input");
+        $data = json_decode($dataForm);
+
+        //        On récupère le GET envoyé dans l'url
+        $id = $request->get('id');
+
+        //        On récupère l'Entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        //        On récupère le repository Student
+        $student = $em->getRepository('AppBundle:Student')->findOneBy(array('id' => $id));
+
+        //        On récupère les skills associés à l'étudiant
+        $infoSkill = $student->getSkills();
+
+        //        On supprime toutes les compétences de l'eleve
+        foreach ($infoSkill as $skillDelete) {
+            $student->removeSkill($skillDelete);
+        }
+
+        //        On définit les variables avec ce que l'on récupère du JSON'
+        $firstname = $data->firstname;
+        $lastname = $data->lastname;
+        $gender = $data->gender;
+        $birthDate = $data->birthDate;
+        $address = $data->address;
+        $phone = $data->phone;
+        $email = $data->email;
+        $emergencyContact = $data->emergencyContact;
+        $github = $data->github;
+        $linkedIn = $data->linkedin;
+        $personalProject = $data->personalProject;
+        $photo = $data->photo;
+        $newSkills = $data->skills;
+
+        //        Si les données existent et sont differentes de null, on modifie l'eleve dans la DB, sinon on ne fait rien
+        if (isset($firstname) && ($firstname) != null)
+            $student->setFirstname($firstname);
+
+        if (isset($lastname) && ($lastname) != null)
+            $student->setLastname($lastname);
+
+        if (isset($gender) && ($gender) != null)
+            $student->setGender($gender);
+
+        if (isset($birthDate) && ($data->birthDate) != null)
+            $student->setBirthDate(new \DateTime($birthDate));
+
+        if (isset($address) && ($address) != null)
+            $student->setAddress($address);
+
+        if (isset($phone) && ($phone) != null)
+            $student->setPhone($data->phone);
+
+        if (isset($email) && ($email) != null)
+            $student->setEmail($email);
+
+        if (isset($emergencyContact) && ($emergencyContact) != null)
+            $student->setEmergencyContact($emergencyContact);
+
+        if (isset($github) && ($github) != null)
+            $student->setGithub($github);
+
+        if (isset($linkedIn) && ($linkedIn) != null)
+            $student->setLinkedin($linkedIn);
+
+        if (isset($personalProject) && ($personalProject) != null)
+            $student->setPersonalProject($personalProject);
+
+        if (isset($photo) && ($photo) != null)
+            $student->setPhoto($photo);
+
+        //        Pour chaque id de competence récupéré, on va chercher la compétence dans la table skill et on l'ajoute à l'eleve
+        foreach ($newSkills as $skillStudent) {
+            $skill = $em->getRepository('AppBundle:Skill')->findOneBy(array('id' => $skillStudent));
+            $student->addSkill($skill);
+        };
+
+        //        On met a jour dans la DB
+        $em->flush();
+
+        //        Une fois effacement terminé, on verifie si $studentDelete existe encore ou pas dans l'em
+        return ($student) ? new JsonResponse(array('response' => "student update")) : new JsonResponse(array('response' => "student dead"));
+    }
+
+    /**
      * @Route("/student/delete/{id}" , name="deleteStudent")
      * Suppression d'un student
      */
